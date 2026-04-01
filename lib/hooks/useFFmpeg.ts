02d0d98aa637f31f5d8a1fdd9ce572b2f2a43935
KwakSangOk <dolgury@gmail.com>
2026-04-01 16:31:07 +0900

    feat: ffmpeg 오류 수정

--- lib/hooks/useFFmpeg.ts
+++ lib/hooks/useFFmpeg.ts
@@ -49,7 +49,7 @@ export function useFFmpeg() {
     ])
 
     const data = await ffmpeg.readFile('output.mp4')
-    return new Blob([data], { type: 'video/mp4' })
+    return new Blob([data as Uint8Array], { type: 'video/mp4' })
   }
 
   return { composeVideo, progress, loaded }
